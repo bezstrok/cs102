613e99d22f291f8246142dab245a80a28749337e
Malaev_Stepan <Pocandre87@gmail.com>
2023-08-30 18:57:44 +0300

    set config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.l_BqKQTczoW-yI_kZJQVTdDFqVNh1bjCGreNj6Zve6T0w5gBmBYKLcncmiNj6D02slEBK5iq-ybEOzThrVu_sbgIRCIH-IyOIjWsf1BVE40IcPlqdffzoAJOZMH1aMe0OabMJw-idJPvfKXb8_3V4lEX1NRxwozH_d2sDbFjenmJoo63QlnH_BzKy8xNCJxv",
     "version": "5.126",
 }
