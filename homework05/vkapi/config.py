c9cc458b13c6746c9b9e6c1e4155c5475cbd0357
Malaev_Stepan <Pocandre87@gmail.com>
2023-08-30 21:01:54 +0300

    fix config domain

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -1,7 +1,7 @@
 # https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
 
 VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
+    "domain": "https://api.vk.com/method/",
     "access_token": "vk1.a.l_BqKQTczoW-yI_kZJQVTdDFqVNh1bjCGreNj6Zve6T0w5gBmBYKLcncmiNj6D02slEBK5iq-ybEOzThrVu_sbgIRCIH-IyOIjWsf1BVE40IcPlqdffzoAJOZMH1aMe0OabMJw-idJPvfKXb8_3V4lEX1NRxwozH_d2sDbFjenmJoo63QlnH_BzKy8xNCJxv",
     "version": "5.126",
 }
