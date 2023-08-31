6b25575871ca8dfdbd0bf130c002bb0b95819e22
Malaev_Stepan <Pocandre87@gmail.com>
2023-09-01 02:01:22 +0300

    implement age_predict function in age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -1,7 +1,10 @@
 import datetime as dt
 import statistics
 import typing as tp
+from pprint import pprint
+from time import perf_counter
 
+from vkapi.exceptions import APIError
 from vkapi.friends import get_friends
 
 
@@ -14,4 +17,21 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    try:
+        people = get_friends(user_id, fields=["bdate"]).items
+    except APIError:
+        return None
+
+    today = dt.date.today()
+
+    ages = []
+    for p in people:  # formats may be "dd.mm.yyyy" or "dd.mm" or doesn't exist
+        if isinstance(p, dict) and "bdate" in p and p["bdate"].count(".") == 2:
+            d, m, y = map(int, p["bdate"].split("."))
+            age = today.year - y - ((today.month, today.day) < (m, d))
+            ages.append(age)
+
+    if not ages:
+        return None
+
+    return statistics.median(ages)
