48edc03d8f936d1aa94dbf8194fb5aab6b389b4e
Malaev_Stepan <Pocandre87@gmail.com>
2023-09-04 22:30:49 +0300

    Fix test_wall.py response data format and response count check

--- homework05/tests/tests_api/test_wall.py
+++ homework05/tests/tests_api/test_wall.py
@@ -25,10 +25,12 @@ class GetWallTestCase(unittest.TestCase):
             responses.POST,
             "https://api.vk.com/method/execute",
             json={
-                "response": {
-                    "count": 1,
-                    "items": expected_items,
-                }
+                "response": [
+                    {
+                        "count": 1,
+                        "items": expected_items,
+                    }
+                ]
             },
             status=200,
         )
@@ -45,7 +47,9 @@ class GetWallTestCase(unittest.TestCase):
         )
         resp_body = unquote(responses.calls[0].request.body)
         self.assertTrue(
-            '"count":"1"' in resp_body or '"count":+"1"' in resp_body,
+            '"count":"1"' in resp_body
+            or '"count":+"1"' in resp_body
+            or ("var+count+=+1;" in resp_body and '"count":+count'),
             msg="Вы должны сделать один запрос, чтобы узнать общее число записей",
         )
 
@@ -55,16 +59,17 @@ class GetWallTestCase(unittest.TestCase):
             responses.POST,
             "https://api.vk.com/method/execute",
             json={
-                "response": {
-                    "count": 6000,
-                    "items": [],
-                }
+                "response": [
+                    {
+                        "count": 6000,
+                        "items": [],
+                    }
+                ]
             },
             status=200,
         )
         start = time.time()
         with patch("vkapi.wall.get_posts_2500") as get_posts_2500:
-            get_posts_2500.return_value = []
             _ = get_wall_execute(domain="cs102py", count=6000)
         end = time.time()
         self.assertGreaterEqual(end - start, 2.0, msg="Слишком много запросов в секунду")
